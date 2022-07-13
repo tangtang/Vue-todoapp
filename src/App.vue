f003cbe6b67a8b28a658045eb93411c97f42b141
Tang Tang <T370436391@outlook.com>
2022-07-13 14:56:13 +0800

    fifth commit
    filter todoitems

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@
     <div class="container">
       <h1>欢迎使用 orange 待办事项</h1>
       <todo-add :tid="todos.length" @add-todo="addTodo"/>
-      <todo-filter />
-      <todo-list :todos="todos" />
+      <!-- 过滤选项传递给todo-filter组件，监听change-filter属性 -->
+      <todo-filter :selected="filter" @change-filter="filter = $event" />
+      <!-- 显示过滤后的数组 -->
+      <todo-list :todos="filteredTodos" />
     </div>
   </main>
 </template>
 
 <script>
-import { ref }  from "vue";
+import { computed,ref }  from "vue";
 import TodoAdd from './components/TodoAdd.vue';
 import TodoFilter from './components/TodoFilter.vue';
 import TodoList from './components/TodoList.vue';
@@ -21,9 +23,26 @@ export default {
   setup (){
     const todos = ref([]);//包装一个空数组，来作为默认todo列表的数据
     const addTodo = (todo) => todos.value.push(todo);
-    return { //在temp中使用函数和数据，要以对象的新式返回
+
+    const filter = ref("all");//选项默认为all
+
+    //根据filter的值过滤列表，把过滤好的数组放到filter.todos变量中
+    const filteredTodos = computed(() => {
+      switch (filter.value){
+        case "done":
+          return todos.value.filter((todo) => todo.completed);
+        case "todo":
+         return todos.value.filter((todo) => !todo.completed)
+         default:
+          return todos.value;
+      }
+    });
+  
+    return { //在temp中使用函数和数据，要以对象的形式返回
       todos,
       addTodo,
+      filter,
+      filteredTodos,
     };
   },
 };
--- src/components/TodoFilter.vue
+++ src/components/TodoFilter.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="filters">
-        <span class="filter active">All</span>
-        <span class="filter">Done</span>
-        <span class="filter">Undone</span>
-      </div>
+    <span 
+    v-for="filter in filters" 
+    :key="filter.value"
+    @click="$emit('change-filter',filter.value)" 
+    class="filter"
+    :class=" {active: selected ===filter.value}">
+      {{ filter.label }}
+    </span>
+  </div>
 </template>
 
 <script>
 export default {
-name:"TodoFilter"
-}
+  name: "TodoFilter",
+  props:["seleted"],
+  setup() {
+    const filters = [
+      { label: "全部", value: "all" },
+      { label: "已完成", value: "done" },
+      { label: "未完成", value: "todo" },
+    ];
+    return { filters };
+  },
+};
 </script>
 
 <style>
-
 .filters {
   display: flex;
   margin: 24px 2px;
@@ -24,6 +37,7 @@ name:"TodoFilter"
 .filters .filter {
   margin-right: 14px;
   transition: 0.8s;
+  cursor: pointer;
 }
 
 .filters .filter.active {
